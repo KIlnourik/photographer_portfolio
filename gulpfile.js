70916cc834bb3896056540c20fe2cc1d1598c62a
KIlnourik <kilnur1988@rambler.ru>
2021-04-06 17:16:33 +0500

    relocated css and scss directory for correct gulp working, started style for navigation menu, rewrite normalize.css in scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
@@ -9,7 +11,7 @@ const sync = require("browser-sync").create();
 // Styles
 
 const styles = () => {
-  return gulp.src("source/styles/sass/style.scss")
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
@@ -17,11 +19,12 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/styles/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
 const server = (done) => {
   sync.init({
     server: {
