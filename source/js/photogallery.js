d6d8423ca865ea4316072e7b95fd46a03f80e3b3
KIlnur <kilnur1988@rambler.ru>
2023-08-03 10:35:50 +0500

    Исправляет все недочеты. Сборка рабочая на Sass и gulp

--- source/js/photogallery.js
+++ source/js/photogallery.js
@@ -1,4 +1,6 @@
 'use strict';
 
+import './modules/menu-nav.js';
 import './modules/modal.js';
+import './modules/swiper.js';
 import './modules/up-link.js';
--- source/js/reviews.js
+++ source/js/reviews.js
@@ -1,5 +1,5 @@
 'use strict';
 
+import './modules/menu-nav.js';
 import './modules/up-link.js';
 import './modules/show-more.js';
-
